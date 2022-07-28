e3c3fa23c4628b317e71dfb6ac4cdf7bb1ea500f
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-28 23:20:12 +0300

    My template

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ app = Flask(__name__)
 @app.route('/home')
 def index():
     return "Hello World!"
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User_page" + name + '-' + id
+#
+# @app.route('/user/<string:name>/<int:id>')
+# def user(name, id):
+#     return "User_page" + name + '-' + id
 
 
 @app.route('/about')
 def about():
     return "About Us"
+
+
+@app.route('/home_work')
+def home_work():
+    return "Is that home work correct?"
 
 if __name__ == '__main__':
     app.run(debug=True)
